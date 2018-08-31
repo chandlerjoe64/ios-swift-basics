1a3fb4a7badd406401a16bebdce5770981a654ea
Joe Chandler <chandlerjoe64@yahoo.com>
2018-08-31 16:29:57 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Joseph Chandler on 8/31/18.
+//  Copyright © 2018 Joseph Chandler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
